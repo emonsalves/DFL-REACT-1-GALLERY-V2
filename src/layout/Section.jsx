b113253f2b1b77a8a184884b67d9c641713d2a65
Enzo Monsalves <ingeniero.monsalves@gmail.com>
2022-08-17 00:08:07 -0400

    Feat: Components Layout and Style Basic

---
+++ src/layout/Section.jsx
@@ -0,0 +1,21 @@
+import Card from "../components/Card"
+function Section() {
+  return (
+    <div className ="flex flex-wrap mt-10 pt-10 m-1 gap-3 justify-center">
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  )
+}
+
+export default Section
