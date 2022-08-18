55988687715eb88fd5221a050783616b5803b29d
Enzo Monsalves <ingeniero.monsalves@gmail.com>
2022-08-18 09:48:57 -0400

    Fix : Bugs class to className, variables and layout

--- src/App.js
+++ src/App.js
@@ -6,11 +6,70 @@ import Section from './layout/Section';
 
 function App() {
 
+  const digimons = [{
+    "name": "Greymon",
+    "img": "https://digimon.shadowsmith.com/img/greymon.jpg",
+    "level": "Champion"
+  }, {
+    "name": "SkullGreymon",
+    "img": "https://digimon.shadowsmith.com/img/skullgreymon.jpg",
+    "level": "Ultimate"
+  },
+  {
+    "name": "MetalGreymon",
+    "img": "https://digimon.shadowsmith.com/img/metalgreymon.jpg",
+    "level": "Ultimate"
+  },
+  {
+    "name": "WarGreymon",
+    "img": "https://digimon.shadowsmith.com/img/wargreymon.jpg",
+    "level": "Mega"
+  },
+  {
+    "name": "Angemon",
+    "img": "https://digimon.shadowsmith.com/img/angemon.jpg",
+    "level": "Champion"
+  },
+  {
+    "name": "Angewomon",
+    "img": "https://digimon.shadowsmith.com/img/angewomon.jpg",
+    "level": "Ultimate"
+  },
+  {
+    "name": "MagnaAngemon",
+    "img": "https://digimon.shadowsmith.com/img/magnaangemon.jpg",
+    "level": "Ultimate"
+  }, {
+    "name": "MarineAngemon",
+    "img": "https://digimon.shadowsmith.com/img/marineangemon.jpg",
+    "level": "Mega"
+  }, {
+    "name": "Agumon",
+    "img": "https://digimon.shadowsmith.com/img/agumon.jpg",
+    "level": "Rookie"
+  }, {
+    "name": "Gabumon",
+    "img": "https://digimon.shadowsmith.com/img/gabumon.jpg",
+    "level": "Rookie"
+  }, {
+    "name": "Garurumon",
+    "img": "https://digimon.shadowsmith.com/img/garurumon.jpg",
+    "level": "Champion"
+  }, {
+    "name": "WereGarurumon",
+    "img": "https://digimon.shadowsmith.com/img/weregarurumon.jpg",
+    "level": "Ultimate"
+  }, {
+    "name": "MetalGarurumon",
+    "img": "https://digimon.shadowsmith.com/img/metalgarurumon.jpg",
+    "level": "Mega"
+  }
+  ]
   return (
-    <div>      
-      <Header/>
-      <Section/>
-      <Footer/>
+    <div>
+      <Header />
+      <Section digimons={digimons} />
+      <Footer />
     </div>
   );
 }
--- src/layout/Section.jsx
+++ src/layout/Section.jsx
@@ -1,19 +1,19 @@
 import Card from "../components/Card"
-function Section() {
+function Section({digimons}) {
   return (
     <div className ="flex flex-wrap mt-10 pt-10 m-1 gap-3 justify-center">
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      <Card digimons={digimons[0]}/>
+      <Card digimons={digimons[1]}/>
+      <Card digimons={digimons[2]}/>
+      <Card digimons={digimons[3]}/>
+      <Card digimons={digimons[4]}/>
+      <Card digimons={digimons[5]}/>
+      <Card digimons={digimons[6]}/>
+      <Card digimons={digimons[7]}/>
+      <Card digimons={digimons[8]}/>
+      <Card digimons={digimons[9]}/>
+      <Card digimons={digimons[10]}/>
+      <Card digimons={digimons[11]}/>
     </div>
   )
 }
